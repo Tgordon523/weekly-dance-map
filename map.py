bfc92b33bb43e715259e118ba2acc429a301a29b
Tim <tgordon523@gmail.com>
2025-03-18 20:11:04 -0500

    testing plotly plot

--- map.py
+++ map.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 import pathlib
 
+import plotly.express as px
 import leafmap.foliumap as leafmap
 
 # Adjust the width of the Streamlit page
@@ -73,7 +74,7 @@ def fetch_data():
 
 
 df = fetch_data()
-st.dataframe(df)
+# st.dataframe(df)
 # df.sort_values(by="", inplace=True)
 # Setup two filters (will change set-up later)
 col1, col2 = st.columns(2)
@@ -96,15 +97,15 @@ if city_filter:
     # print(df.day.nunique())
     city_mask = df["City"].isin(city_filter)
     df = df.loc[city_mask]
-    t = df.days.unique()
-    subdict = {x: day_palette_dict[x] for x in t if x in day_palette_dict}
-    new_colors = [k for k in subdict.values()]
-    st.write(day_palette_dict, new_colors)
-    day_palette = new_colors
+    # t = df.days.unique()
+    # subdict = {x: day_palette_dict[x] for x in t if x in day_palette_dict}
+    # new_colors = [k for k in subdict.values()]
+    # st.write(day_palette_dict, new_colors)
+    # day_palette = new_colors
     # st.write(day_palette)
 
 # st.dataframe(df)
-m = leafmap.Map(center=(36, -80),zoom=4, measure_control=False, draw_control=False)
+# m = leafmap.Map(center=(36, -80),zoom=4, measure_control=False, draw_control=False)
 # cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
 # regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 states = "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
@@ -112,24 +113,96 @@ states = "https://raw.githubusercontent.com/python-visualization/folium-example-
 # m.add_geojson(states, layer_name="US States")
 # Cities points
 df.sort_values(by="weekday", inplace=True)
-m.add_points_from_xy(
-    df[["days", "Time", "Title", "Venue", "Address", "Cost", "Longitude", "Latitude"]],
-    x="Longitude",
-    y="Latitude",
-    color_column="days",
-    prefix='glyphicon',
-    marker_colors=day_palette,
-    add_legend=True,
+df['color'] = df['days'].map(day_palette_dict)
+df["size"] = 40
+
+fig = px.scatter_map(
+    df,
+    lat='Latitude',
+    lon='Longitude',
+    color='days',
+    size='size',
+    labels={"City":"City", "Address":"Address"},
+    size_max=40,
+    hover_name='Venue',
+    # hover_data={'value': True, 'day': True, 'lat': False, 'lon': False},
+    color_discrete_map=day_palette_dict,
+    # projection='natural earth',
+    # scope='north america',
+    zoom=5,
+    # title='North America Cluster Map'
+)
+fig.update_traces(cluster=dict(enabled=True))
+# Configure the layout with a mapbox style
+# fig.update_layout(
+#     title='North America Map',
+#     geo=dict(
+#         scope='north america',
+#         projection_type='natural earth',
+#         showland=True,
+#         landcolor='rgb(243, 243, 243)',
+#         countrycolor='rgb(204, 204, 204)',
+#         coastlinecolor='rgb(204, 204, 204)',
+#         lakecolor='rgb(187, 222, 251)',
+#         showocean=True,
+#         oceancolor='rgb(72, 78, 82)',
+#         showrivers=True,
+#         rivercolor='rgb(187, 222, 251)',
+#         showcountries=True,
+#         showsubunits=True,
+#         showframe=False,
+#         resolution=110
+#     ),
+#     width=1200,
+#     height=800,
+#     margin=dict(l=0, r=0, b=0, t=30)
+# )
+
+# m.add_points_from_xy(
+#     df[["days", "Time", "Title", "Venue", "Address", "Cost", "Longitude", "Latitude"]],
+#     x="Longitude",
+#     y="Latitude",
+#     color_column="days",
+#     prefix='glyphicon',
+#     marker_colors=day_palette,
+#     add_legend=True,
+# )
+fig.update_layout(
+    legend=dict(
+        x=0,
+        y=1,
+        # traceorder="reversed",
+        # title_font_family="Times New Roman",
+        font=dict(
+            family="Courier",
+            size=12,
+            color="black"
+        ),
+        bgcolor="LightSteelBlue",
+        bordercolor="Black",
+        borderwidth=2
+    )
 )
 
-# st.cache_resource
-m.to_streamlit(height=700)
 
+
+
+# st.cache_resourcecenter=(36, -80),zoom=4
+# st.session_state.mapfig.add_trace(go.Scattermapbox(lat=latitude, lon=lons, mode=lines)
+# st.session_state.mapfig.update_layout(mapbox={'style':"open-street-map",'center':{'lon':30,'lat':-80},'zoom':4})
+fig.update_geos(fitbounds="locations")
+fig.update_layout(map_style="open-street-map")
+fig.update_layout(height=700, margin={"r":0,"t":0,"l":0,"b":0})
+
+st.plotly_chart(fig,height=700)
+# st.plotly_chart(fig)
+# st.plotly_chart(fig,height=1200, zoom=5)
+# m.to_streamlit(height=700)
+# st.write(df.columns)
 col1_bar, col2_bar = st.columns(2)
-st.write()
 
 col1_bar.bar_chart(df.days.value_counts().reset_index().sort_values(by="days"), x="days", y='count', x_label="Events per Day", horizontal=True)
-col2_bar.bar_chart(df.City.value_counts().reset_index().sort_values(by="City"), x="days", y='count', x_label="Events per Day", horizontal=True)
+col2_bar.bar_chart(df.City.value_counts().reset_index().sort_values(by="City"), x="City", y='count', x_label="Events per Day", horizontal=True)
 # st.write(df.groupby("Venue")["Title"].count())
 
 st.write(
