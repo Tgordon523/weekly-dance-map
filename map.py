18b880e9efe89a0049f08215ee4f40638b611883
Tim <tgordon523@gmail.com>
2025-03-19 19:32:20 -0500

    grouped df, sidebar, format

--- map.py
+++ map.py
@@ -8,9 +8,7 @@ import leafmap.foliumap as leafmap
 
 # Adjust the width of the Streamlit page
 st.set_page_config(
-    page_title="Only Weeklies",
-    layout="wide",
-    initial_sidebar_state="expanded"
+    page_title="Only Weeklies", layout="wide", initial_sidebar_state="expanded"
 )
 
 # Add Title
@@ -40,14 +38,14 @@ day_palette = [
 ]
 
 day_palette_dict = {
-    "Monday":"darkred",
-    "Tuesday":"red",
-    "Wednesday":"orange",
-    "Thursday":"green",
-    "Friday":"darkgreen",
-    "Saturday":"blue",
-    "Sunday":"darkblue",
-    #    '#d53e4f'
+    "Monday": "#3288bd",
+    "Tuesday": "#66c2a5",
+    "Wednesday": "#abdda4",
+    "Thursday": "#e6f598",
+    "Friday": "#fee08b",
+    "Saturday": "#fdae61",
+    "Sunday": "#f46d43",
+    "Multiple": "#d53e4f",
 }
 
 
@@ -74,29 +72,27 @@ def fetch_data():
 
 
 df = fetch_data()
-# st.dataframe(df)
-# df.sort_values(by="", inplace=True)
-# Setup two filters (will change set-up later)
-col1, col2 = st.columns(2)
+df["size"] = 40
 
-default_city_selection = (
-    df[df.City.isin(st.session_state.City)].City.unique()
-    if "City" in st.session_state
-    else None
-)
-city_filter = col1.multiselect(
-    label="select cities",
-    options=df.City.unique(),
-    default=default_city_selection,
-    key="City",
-)
-
+# col1, _, _ = st.columns(3)
+with st.sidebar:
+    default_city_selection = (
+        df[df.City.isin(st.session_state.City)].City.unique()
+        if "City" in st.session_state
+        else None
+    )
+    city_filter = st.multiselect(
+        label="select cities",
+        options=df.City.unique(),
+        default=default_city_selection,
+        key="City",
+    )
 
 
-if city_filter:
-    # print(df.day.nunique())
-    city_mask = df["City"].isin(city_filter)
-    df = df.loc[city_mask]
+    if city_filter:
+        # print(df.day.nunique())
+        city_mask = df["City"].isin(city_filter)
+        df = df.loc[city_mask]
     # t = df.days.unique()
     # subdict = {x: day_palette_dict[x] for x in t if x in day_palette_dict}
     # new_colors = [k for k in subdict.values()]
@@ -112,106 +108,91 @@ states = "https://raw.githubusercontent.com/python-visualization/folium-example-
 
 # m.add_geojson(states, layer_name="US States")
 # Cities points
+grouped_days_df = (
+    (df.groupby("Venue")["Title"].count().reset_index())
+    .merge(
+        (df.groupby("Venue")["days"].apply(lambda x: "%s" % ", ".join(x)).reset_index())
+    )
+    .merge(
+        (df.groupby("Venue")["Time"].apply(lambda x: "%s" % ", ".join(x)).reset_index())
+    )
+)
+
+grouped_days_df_complete = grouped_days_df.merge(
+    df[["Venue", "City", "Latitude", "Longitude", "size", "weekday"]], on="Venue", how="left"
+).drop_duplicates(subset="Venue")
+
+grouped_days_df_complete["Marker"] = np.where(
+    grouped_days_df_complete.Title > 1, "Multiple", grouped_days_df_complete.days
+)
+grouped_days_df_complete["weekday"] = np.where(
+    grouped_days_df_complete.Title > 1, 8, grouped_days_df_complete.weekday
+)
+grouped_days_df_complete.sort_values(by="weekday", inplace=True)
+# st.dataframe(grouped_days_df_complete)
+
 df.sort_values(by="weekday", inplace=True)
-df['color'] = df['days'].map(day_palette_dict)
-df["size"] = 40
+grouped_days_df_complete["color"] = grouped_days_df_complete["Marker"].map(
+    day_palette_dict
+)
 
 fig = px.scatter_map(
-    df,
-    lat='Latitude',
-    lon='Longitude',
-    color='days',
-    size='size',
-    labels={"City":"City", "Address":"Address"},
+    grouped_days_df_complete,
+    lat="Latitude",
+    lon="Longitude",
+    color="Marker",
+    size="size",
+    labels={"City": "City", "Address": "Address"},
     size_max=40,
-    hover_name='Venue',
+    hover_name="Venue",
+    color_discrete_sequence=px.colors.qualitative.G10,
+    # color_continuous_scale=["green", "blue"],
     # hover_data={'value': True, 'day': True, 'lat': False, 'lon': False},
-    color_discrete_map=day_palette_dict,
-    # projection='natural earth',
-    # scope='north america',
-    zoom=5,
-    # title='North America Cluster Map'
+    zoom=4.5,
+    title="North America Weeklies",
 )
 fig.update_traces(cluster=dict(enabled=True))
-# Configure the layout with a mapbox style
-# fig.update_layout(
-#     title='North America Map',
-#     geo=dict(
-#         scope='north america',
-#         projection_type='natural earth',
-#         showland=True,
-#         landcolor='rgb(243, 243, 243)',
-#         countrycolor='rgb(204, 204, 204)',
-#         coastlinecolor='rgb(204, 204, 204)',
-#         lakecolor='rgb(187, 222, 251)',
-#         showocean=True,
-#         oceancolor='rgb(72, 78, 82)',
-#         showrivers=True,
-#         rivercolor='rgb(187, 222, 251)',
-#         showcountries=True,
-#         showsubunits=True,
-#         showframe=False,
-#         resolution=110
-#     ),
-#     width=1200,
-#     height=800,
-#     margin=dict(l=0, r=0, b=0, t=30)
-# )
 
-# m.add_points_from_xy(
-#     df[["days", "Time", "Title", "Venue", "Address", "Cost", "Longitude", "Latitude"]],
-#     x="Longitude",
-#     y="Latitude",
-#     color_column="days",
-#     prefix='glyphicon',
-#     marker_colors=day_palette,
-#     add_legend=True,
-# )
 fig.update_layout(
     legend=dict(
         x=0,
         y=1,
         # traceorder="reversed",
         # title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
+        font=dict(family="Courier", size=12, color="black"),
         bgcolor="LightSteelBlue",
         bordercolor="Black",
-        borderwidth=2
+        borderwidth=2,
     )
 )
-
-
-
-
-# st.cache_resourcecenter=(36, -80),zoom=4
-# st.session_state.mapfig.add_trace(go.Scattermapbox(lat=latitude, lon=lons, mode=lines)
-# st.session_state.mapfig.update_layout(mapbox={'style':"open-street-map",'center':{'lon':30,'lat':-80},'zoom':4})
 fig.update_geos(fitbounds="locations")
 fig.update_layout(map_style="open-street-map")
-fig.update_layout(height=700, margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(height=700, margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
-st.plotly_chart(fig,height=700)
+st.plotly_chart(fig, height=700)
+st.text("")
+st.text("")
+st.text("")
 # st.plotly_chart(fig)
 # st.plotly_chart(fig,height=1200, zoom=5)
 # m.to_streamlit(height=700)
 # st.write(df.columns)
 col1_bar, col2_bar = st.columns(2)
 
-col1_bar.bar_chart(df.days.value_counts().reset_index().sort_values(by="days"), x="days", y='count', x_label="Events per Day", horizontal=True)
-col2_bar.bar_chart(df.City.value_counts().reset_index().sort_values(by="City"), x="City", y='count', x_label="Events per Day", horizontal=True)
-# st.write(df.groupby("Venue")["Title"].count())
-
-st.write(
-    (df.groupby("Venue")["Title"].count().reset_index()).merge(
-        (df.groupby("Venue")["days"].apply(lambda x: "%s" % ", ".join(x)).reset_index())
-    )
+col1_bar.bar_chart(
+    df.days.value_counts().reset_index().sort_values(by="days"),
+    x="days",
+    y="count",
+    x_label="Events per Day",
+    horizontal=True,
 )
-
-# st.write(df.groupby("Venue")["days"].apply(lambda x: "%s" % ", ".join(x)))
+col2_bar.bar_chart(
+    df.City.value_counts().reset_index().sort_values(by="City"),
+    x="City",
+    y="count",
+    x_label="Events per Day",
+    horizontal=True,
+)
 
 st.write(df[["days", "Time", "Venue", "Address"]])
 
